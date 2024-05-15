af5eb7b7405f6d6f3a7078c83134fbd17d49e4a2
Gadiel02 <gadielrivera02@gmail.com>
2024-05-14 22:20:30 -0400

    test4

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[x**2 for x in row] for row in matrix]
+    new_matrix = [row[:] for row in matrix]
+    for lists in new_matrix:
+        for idx in range(0, len(lists)):
+            lists[idx] = lists[idx]**2
+    return new_matrix

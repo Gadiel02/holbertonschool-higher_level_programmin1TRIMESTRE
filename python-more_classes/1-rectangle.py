e1efafc0841a642e839e1bb4ad0231a769b5490e
Gadiel02 <gadielrivera02@gmail.com>
2024-05-21 21:48:11 -0400

    test

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,9 +1,38 @@
 #!/usr/bin/python3
-Rectangle = __import__('1-rectangle').Rectangle
+"""
+This is the "Rectangle"  module.
 
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
+This module provides a simple Rectangle class with attribute width and height.
+Default values of both attributes are 0.
+"""
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
+
+class Rectangle:
+    """A Rectangle class with attributes  width and height"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,101 +1,47 @@
 #!/usr/bin/python3
-""" Write a class Rectangle that defines a rectangle
+"""
+This is the "Rectangle"  module.
+
+This module provides a simple Rectangle class.
 """
 
 
 class Rectangle:
-    """creates class that defines a rectangle
-    Attributes:
-    empty
+    """A Rectangle class with attributes width and height, and
+    methods area and perimiter.
     """
-
     def __init__(self, width=0, height=0):
-        """ method to initialize a Rectangle
-
-        Attribute:
-            width (int): optional, 0 by default
-            height (int): optional, 0 by default
-        """
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """
-        gets width
-
-        Returns:
-            width (int)
-        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        sets width of the rectangle
-
-        Attributes:
-            width (int)
-
-        Raises:
-            TypeError: width is not an integer
-            ValueError: width is negative
-        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
-        """
-        gets height
-        Returns:
-           height (int)
-        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        sets height of the rectangle
-
-        Attributes:
-            height (int)
-
-        Raises:
-            TypeError: height is not an integer
-            ValueError: height is negative
-        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
-        """Public instance method that returns the rectangle area
-
-        Attributes:
-            area (int)
-
-        Returns:
-        area (int): area of the rectangle
-        """
-        return self.__height * self.__width
+        return self.__width * self.__height
 
     def perimeter(self):
-        """Public instance method that returns the rectangle perimeter
-        Attributes:
-            perimeter (int): perimeter of the rectangle
-
-        Returns:
-            perimeter (int): 0 if height or width = 0
-        """
-        if self.__width == 0 or self.__height == 0:
+        if self.__width is 0 or self.__height is 0:
             return 0
-        else:
-            return 2 * self.__height + 2 * self.__width
+        return (2 * self.__width) + (2 * self.__height)

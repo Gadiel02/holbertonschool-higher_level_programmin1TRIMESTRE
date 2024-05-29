467850bf003ff96fe6dcd1c6fc3d0dc9bf425050
Gadiel02 <gadielrivera02@gmail.com>
2024-05-28 22:41:33 -0400

    test

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Defines a string-to-JSON function."""
+"""Defines a function to save an object to a JSON file."""
+
 import json
 
-
-def to_json_string(my_obj):
-    """Returns the JSON representation of a string object.
+def save_to_json_file(my_obj, filename):
+    """Save the object to a JSON file.
 
     Args:
-        my_obj: The object to convert to JSON.
-
-    Returns:
-        str: The JSON representation of the object.
+        my_obj: The object to save.
+        filename (str): The name of the file to save to.
     """
-    with open('output.json', 'w') as json_file:
+    with open(filename, 'w') as json_file:
         json.dump(my_obj, json_file)
-

b3d4b8cbd9f69b2fa4a824d8127f75e5771cd2d1
Gadiel02 <gadielrivera02@gmail.com>
2024-05-28 22:37:52 -0400

    test

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,5 +4,12 @@ import json
 
 
 def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
+    """Returns the JSON representation of a string object.
+
+    Args:
+        my_obj: The object to convert to JSON.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
     return json.dumps(my_obj)

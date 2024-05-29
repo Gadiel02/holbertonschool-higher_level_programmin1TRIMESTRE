f2586a0c40af47e230f87ede35d12def479047a9
Gadiel02 <gadielrivera02@gmail.com>
2024-05-28 23:04:35 -0400

    test

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,42 @@
 #!/usr/bin/python3
-"""Defines a JSON file-reading function."""
-import json
-
-
 def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
+    with open(filename, encoding='utf-8') as f:
+        json_str = f.read()
+        return parse_json(json_str)
+
+def parse_json(json_str):
+    stack = []
+    current_object = {}
+    key = ''
+    i = 0
+    while i < len(json_str):
+        if json_str[i] == '{':
+            stack.append(current_object)
+            current_object = {}
+        elif json_str[i] == '}':
+            if stack:
+                parent_object = stack.pop()
+                parent_object[key] = current_object
+                current_object = parent_object
+                key = ''
+        elif json_str[i] == '"':
+            i += 1
+            j = i
+            while json_str[i] != '"':
+                i += 1
+            if not key:
+                key = json_str[j:i]
+            else:
+                current_object[key] = json_str[j:i]
+                key = ''
+        elif json_str[i] == ':':
+            i += 1
+        elif json_str[i] == ',':
+            pass
+        i += 1
+    return current_object
+
+# Testing the function
+# Replace 'filename.json' with the actual filename
+result = load_from_json_file('filename.json')
+print(result)

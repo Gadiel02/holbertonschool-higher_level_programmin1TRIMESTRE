a1177ec97f8be7e3abe9f30cb021bbd012757743
Gadiel02 <gadielrivera02@gmail.com>
2024-05-28 22:39:27 -0400

    test

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -12,4 +12,6 @@ def to_json_string(my_obj):
     Returns:
         str: The JSON representation of the object.
     """
-    return json.dumps(my_obj)
+    with open('output.json', 'w') as json_file:
+        json.dump(my_obj, json_file)
+

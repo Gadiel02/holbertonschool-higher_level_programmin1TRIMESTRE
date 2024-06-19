9110fef651e76df804ff4d5b88c08226ad7ed5ce
Gadiel02 <gadielrivera02@gmail.com>
2024-06-18 21:16:23 -0400

    task 4 to 8

--- 3-force_name.sql
+++ 3-force_name.sql
@@ -1,1 +1,2 @@
-rara
+-- Create tables
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
--- 5-unique_id.sql
+++ 5-unique_id.sql
@@ -1,1 +1,2 @@
-tata
+-- Create tables
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
